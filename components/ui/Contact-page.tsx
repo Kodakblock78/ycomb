2cb1e08418a4c2fcb86212b477aadd3dc3f3a064
Kodakblock78 <ezekielforko875@gmail.com>
2025-03-07 13:34:22 +0000

    add the contact page layout

--- components/ui/Contact-page.tsx
+++ components/ui/Contact-page.tsx
@@ -16,22 +16,16 @@ const   Contactpage = () => {
         <div className="w-1/2 flex items-center justify-center px-4">
           {/* White box around the button */}
           <div className="w-full h-[80vh] bg-white rounded-lg flex items-center justify-center">
-            <Link
-              href="/about"
-              className="px-4 py-2 bg-blue-700 text-white text-lg font-semibold rounded-lg hover:bg-blue-600 transition"
-            >
-              Apply Today
-            </Link>
+            
           </div>
         </div>
 
         {/* Right side - Text */}
         <div className="w-1/2 flex flex-col items-center justify-center text-center px-4">
-          <h1 className="text-5xl font-extrabold leading-tight mb-6">
-            Take over
+          <h1 className="text-5xl text-white font-extrabold leading-tight mb-6 underline decoration-white">
+            Let us know your opinion
           </h1>
           <p className="text-lg mb-8 max-w-lg mx-auto">
-            Start today and take control of your company's growth
           </p>
         </div>
       </div>
